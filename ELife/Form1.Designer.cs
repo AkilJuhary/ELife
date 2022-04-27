c3861a8b0e0538a0896ff7ef6a730e1ed0b344a6
Akil Juhary <akiljuhary1@gmail.com>
2022-04-27 11:33:18 +0530

    Add project files.

---
+++ ELife/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ELife
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUN = new System.Windows.Forms.TextBox();
+            this.labelUserName = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxPW = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxUN
+            // 
+            this.textBoxUN.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxUN.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBoxUN.Location = new System.Drawing.Point(277, 107);
+            this.textBoxUN.Multiline = true;
+            this.textBoxUN.Name = "textBoxUN";
+            this.textBoxUN.Size = new System.Drawing.Size(144, 28);
+            this.textBoxUN.TabIndex = 0;
+            // 
+            // labelUserName
+            // 
+            this.labelUserName.AutoSize = true;
+            this.labelUserName.BackColor = System.Drawing.Color.Transparent;
+            this.labelUserName.Font = new System.Drawing.Font("Microsoft JhengHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUserName.ForeColor = System.Drawing.Color.Gainsboro;
+            this.labelUserName.Location = new System.Drawing.Point(121, 107);
+            this.labelUserName.Name = "labelUserName";
+            this.labelUserName.Size = new System.Drawing.Size(123, 27);
+            this.labelUserName.TabIndex = 1;
+            this.labelUserName.Text = "User Name";
+            this.labelUserName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelUserName.Click += new System.EventHandler(this.Username_Click);
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.BackColor = System.Drawing.Color.Transparent;
+            this.labelPassword.Font = new System.Drawing.Font("Microsoft JhengHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPassword.ForeColor = System.Drawing.Color.Gainsboro;
+            this.labelPassword.Location = new System.Drawing.Point(121, 163);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(107, 27);
+            this.labelPassword.TabIndex = 2;
+            this.labelPassword.Text = "Password";
+            this.labelPassword.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelPassword.Click += new System.EventHandler(this.labelPassword_Click);
+            // 
+            // textBoxPW
+            // 
+            this.textBoxPW.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxPW.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBoxPW.Location = new System.Drawing.Point(277, 163);
+            this.textBoxPW.Multiline = true;
+            this.textBoxPW.Name = "textBoxPW";
+            this.textBoxPW.Size = new System.Drawing.Size(144, 28);
+            this.textBoxPW.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SeaGreen;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(224, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(580, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxPW);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUserName);
+            this.Controls.Add(this.textBoxUN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUN;
+        private System.Windows.Forms.Label labelUserName;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxPW;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ELife/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ELife
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Username_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void labelPassword_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String userName = textBoxUN.Text;
+            String passWord = textBoxPW.Text;
+        }
+    }
+}
