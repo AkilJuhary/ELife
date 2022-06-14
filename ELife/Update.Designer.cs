96e135f77a08349ed3e011cc248da5c2bcb76930
Akil Juhary <akiljuhary1@gmail.com>
2022-06-14 12:57:05 +0530

    Update Section

--- ELife/Update.Designer.cs
+++ ELife/Update.Designer.cs
@@ -28,43 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxGender = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBoxDiagnosis = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.textBoxID = new System.Windows.Forms.TextBox();
             this.labelUserName = new System.Windows.Forms.Label();
-            this.textBoxUN = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBoxDepartment = new System.Windows.Forms.ComboBox();
             this.labelBackBtn = new System.Windows.Forms.Label();
             this.dataGridViewUpdate = new System.Windows.Forms.DataGridView();
-            this.label7 = new System.Windows.Forms.Label();
-            this.textBoxSearchName = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.textBoxSearchID = new System.Windows.Forms.TextBox();
-            this.label9 = new System.Windows.Forms.Label();
             this.buttonUpdate = new System.Windows.Forms.Button();
             this.buttonProceed = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUpdate)).BeginInit();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // comboBoxGender
             // 
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.comboBoxGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxGender.FormattingEnabled = true;
+            this.comboBoxGender.Items.AddRange(new object[] {
             "Male",
             "Female",
             "Transgender"});
-            this.comboBox1.Location = new System.Drawing.Point(683, 308);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(144, 32);
-            this.comboBox1.TabIndex = 26;
+            this.comboBoxGender.Location = new System.Drawing.Point(683, 308);
+            this.comboBoxGender.Name = "comboBoxGender";
+            this.comboBoxGender.Size = new System.Drawing.Size(144, 32);
+            this.comboBoxGender.TabIndex = 26;
             // 
             // label6
             // 
@@ -92,16 +89,16 @@
             this.label5.Text = "Diagnosis";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // textBox4
+            // textBoxDiagnosis
             // 
-            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.textBox4.Location = new System.Drawing.Point(683, 361);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(144, 28);
-            this.textBox4.TabIndex = 22;
+            this.textBoxDiagnosis.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxDiagnosis.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDiagnosis.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBoxDiagnosis.Location = new System.Drawing.Point(683, 361);
+            this.textBoxDiagnosis.Multiline = true;
+            this.textBoxDiagnosis.Name = "textBoxDiagnosis";
+            this.textBoxDiagnosis.Size = new System.Drawing.Size(144, 28);
+            this.textBoxDiagnosis.TabIndex = 22;
             // 
             // label4
             // 
@@ -129,27 +126,27 @@
             this.label3.Text = "Address";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // textBox2
+            // textBoxAddress
             // 
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.textBox2.Location = new System.Drawing.Point(242, 415);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(211, 48);
-            this.textBox2.TabIndex = 19;
+            this.textBoxAddress.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAddress.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBoxAddress.Location = new System.Drawing.Point(242, 415);
+            this.textBoxAddress.Multiline = true;
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(211, 48);
+            this.textBoxAddress.TabIndex = 19;
             // 
-            // textBox1
+            // textBoxID
             // 
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.textBox1.Location = new System.Drawing.Point(242, 360);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(144, 28);
-            this.textBox1.TabIndex = 18;
+            this.textBoxID.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxID.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxID.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBoxID.Location = new System.Drawing.Point(242, 360);
+            this.textBoxID.Multiline = true;
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(144, 28);
+            this.textBoxID.TabIndex = 18;
             // 
             // labelUserName
             // 
@@ -164,16 +161,16 @@
             this.labelUserName.Text = "Name";
             this.labelUserName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // textBoxUN
+            // textBoxName
             // 
-            this.textBoxUN.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBoxUN.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxUN.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.textBoxUN.Location = new System.Drawing.Point(242, 308);
-            this.textBoxUN.Multiline = true;
-            this.textBoxUN.Name = "textBoxUN";
-            this.textBoxUN.Size = new System.Drawing.Size(144, 28);
-            this.textBoxUN.TabIndex = 16;
+            this.textBoxName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxName.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBoxName.Location = new System.Drawing.Point(242, 308);
+            this.textBoxName.Multiline = true;
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(144, 28);
+            this.textBoxName.TabIndex = 16;
             // 
             // label1
             // 
@@ -201,11 +198,11 @@
             this.label2.Text = "ID";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // comboBox2
+            // comboBoxDepartment
             // 
-            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
+            this.comboBoxDepartment.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxDepartment.FormattingEnabled = true;
+            this.comboBoxDepartment.Items.AddRange(new object[] {
             "General ",
             "Haematogoly",
             "Neurology",
@@ -218,10 +215,10 @@
             "Pulmonology",
             "Nephrology",
             "Other"});
-            this.comboBox2.Location = new System.Drawing.Point(683, 415);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(144, 32);
-            this.comboBox2.TabIndex = 32;
+            this.comboBoxDepartment.Location = new System.Drawing.Point(683, 415);
+            this.comboBoxDepartment.Name = "comboBoxDepartment";
+            this.comboBoxDepartment.Size = new System.Drawing.Size(144, 32);
+            this.comboBoxDepartment.TabIndex = 32;
             // 
             // labelBackBtn
             // 
@@ -247,30 +244,6 @@
             this.dataGridViewUpdate.Size = new System.Drawing.Size(766, 117);
             this.dataGridViewUpdate.TabIndex = 36;
             // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.BackColor = System.Drawing.Color.Transparent;
-            this.label7.Font = new System.Drawing.Font("Microsoft JhengHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label7.Location = new System.Drawing.Point(356, 96);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(72, 27);
-            this.label7.TabIndex = 38;
-            this.label7.Text = "Name";
-            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // textBoxSearchName
-            // 
-            this.textBoxSearchName.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBoxSearchName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxSearchName.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.textBoxSearchName.Location = new System.Drawing.Point(456, 96);
-            this.textBoxSearchName.Multiline = true;
-            this.textBoxSearchName.Name = "textBoxSearchName";
-            this.textBoxSearchName.Size = new System.Drawing.Size(144, 28);
-            this.textBoxSearchName.TabIndex = 37;
-            // 
             // label8
             // 
             this.label8.AutoSize = true;
@@ -295,19 +268,6 @@
             this.textBoxSearchID.Size = new System.Drawing.Size(144, 28);
             this.textBoxSearchID.TabIndex = 39;
             // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.BackColor = System.Drawing.Color.Transparent;
-            this.label9.Font = new System.Drawing.Font("Microsoft JhengHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label9.Location = new System.Drawing.Point(313, 96);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(37, 27);
-            this.label9.TabIndex = 41;
-            this.label9.Text = "Or";
-            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // buttonUpdate
             // 
             this.buttonUpdate.BackColor = System.Drawing.Color.YellowGreen;
@@ -321,6 +281,7 @@
             this.buttonUpdate.TabIndex = 42;
             this.buttonUpdate.Text = "Update";
             this.buttonUpdate.UseVisualStyleBackColor = false;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
             // 
             // buttonProceed
             // 
@@ -329,12 +290,13 @@
             this.buttonProceed.Cursor = System.Windows.Forms.Cursors.Hand;
             this.buttonProceed.FlatAppearance.BorderSize = 0;
             this.buttonProceed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonProceed.Location = new System.Drawing.Point(657, 90);
+            this.buttonProceed.Location = new System.Drawing.Point(328, 89);
             this.buttonProceed.Name = "buttonProceed";
             this.buttonProceed.Size = new System.Drawing.Size(125, 35);
             this.buttonProceed.TabIndex = 43;
             this.buttonProceed.Text = "Proceed";
             this.buttonProceed.UseVisualStyleBackColor = false;
+            this.buttonProceed.Click += new System.EventHandler(this.buttonProceed_Click);
             // 
             // Update
             // 
@@ -344,25 +306,22 @@
             this.ClientSize = new System.Drawing.Size(902, 531);
             this.Controls.Add(this.buttonProceed);
             this.Controls.Add(this.buttonUpdate);
-            this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.textBoxSearchID);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.textBoxSearchName);
             this.Controls.Add(this.dataGridViewUpdate);
             this.Controls.Add(this.labelBackBtn);
-            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBoxDepartment);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.comboBoxGender);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBoxDiagnosis);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.textBoxID);
             this.Controls.Add(this.labelUserName);
-            this.Controls.Add(this.textBoxUN);
+            this.Controls.Add(this.textBoxName);
             this.Controls.Add(this.label1);
             this.Name = "Update";
             this.Text = "Update";
@@ -375,26 +334,23 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBoxGender;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBoxDiagnosis;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.TextBox textBoxID;
         private System.Windows.Forms.Label labelUserName;
-        private System.Windows.Forms.TextBox textBoxUN;
+        private System.Windows.Forms.TextBox textBoxName;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBoxDepartment;
         private System.Windows.Forms.Label labelBackBtn;
         private System.Windows.Forms.DataGridView dataGridViewUpdate;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBoxSearchName;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox textBoxSearchID;
-        private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Button buttonUpdate;
         private System.Windows.Forms.Button buttonProceed;
     }
--- ELife/Update.cs
+++ ELife/Update.cs
@@ -32,5 +32,61 @@ namespace ELife
         {
 
         }
+
+        private void buttonProceed_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBoxSearchID.Text))
+            {
+                MessageBox.Show("Insert Fields!");
+            }
+            else
+            {
+                try
+                {
+                    con.Open();
+                    string view_IdName = "SELECT * FROM Patients WHERE ID=" + int.Parse(textBoxSearchID.Text);
+                    SqlDataAdapter adapter = new SqlDataAdapter(view_IdName, con);
+
+                    //Fill Data to datagrid
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    dataGridViewUpdate.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error" + ex);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                string update_query = "UPDATE Patients SET ID='"+ textBoxID.Text + "',Name='" + textBoxName.Text + "',Address='" + textBoxAddress.Text + "',Gender='" + comboBoxGender.Text + "',Diagnosis='" + textBoxDiagnosis.Text + "',Department='" + comboBoxDepartment.Text + "' WHERE ID='"+ textBoxSearchID.Text + "'";
+                SqlDataAdapter adapter= new SqlDataAdapter(update_query, con);
+
+                //Fill Data to Grid
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridViewUpdate.DataSource = dt;
+
+                //Confirm update
+                MessageBox.Show("Successfully Updated");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error"+ex);
+            }
+            finally 
+            {
+                con.Close();
+            }
+        }
     }
 }
